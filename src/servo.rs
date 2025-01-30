0e6676ca64cd3e118c3a9821352d34dd29d3a1f8
Matthew Goodman <matthewpetersg@gmail.com>
2025-01-30 15:15:44 -0800

    Unfinished

--- src/servo.rs
+++ src/servo.rs
@@ -1,8 +1,8 @@
-use std::io;
+use std::{collections::HashMap, io};
 
 use crate::{
     command::Command,
-    hardware::{Address, DriverErrors, Instruction, ID},
+    hardware::{Address, DriverErrors, Instruction, InstructionError, ID},
     response::Response,
     serial,
 };
@@ -14,8 +14,10 @@ use thiserror::Error;
 pub enum ServoError {
     #[error("IO Error: {0}")]
     IO(#[from] std::io::Error),
-    #[error("IO Error: {0}")]
+    #[error("Driver Error: {0}")]
     Driver(#[from] DriverErrors),
+    #[error("Instruction Error: {0}")]
+    Instruction(#[from] InstructionError),
     #[error("A response was expected, but none received.")]
     NoResponse,
 }
@@ -30,29 +32,75 @@ pub struct MoveConfig {
     pub speed: u16,
 }
 
-impl MoveConfig {
-    pub fn with_acceleration(mut self, acceleration: u8) -> Self {
+#[derive(Debug, Clone, Copy)]
+pub struct Order {
+    // /// Valid range [0, 254]
+    // acceleration: Option<u8>,
+    // /// Valid range [0, 4096) maps to [0, 360) degrees
+    // position: Option<u16>,
+    // /// Valid range [0, 4096)
+    // speed: Option<u16>,
+    values: HashMap<Address, u8>,
+}
+
+impl Order {
+    pub fn with_acceleration(mut self, acceleration: Option<u8>) -> Self {
         self.acceleration = acceleration;
         self
     }
 
-    pub fn with_position_raw(mut self, position: u16) -> Self {
+    pub fn with_position_raw(mut self, position: Option<u16>) -> Self {
         self.position = position;
         self
     }
 
-    pub fn with_position<T: Angle<f32>>(self, angle: T) -> Self {
-        let position = (angle.to_deg().as_value() * 4096.0 / 360.0)
-            .clamp(0.0, 4095.0)
-            .round() as u16;
+    pub fn with_position<T: Angle<f32>>(self, angle: Option<T>) -> Self {
+        let position = angle.map(|angle| {
+            (angle.to_deg().as_value() * 4096.0 / 360.0)
+                .clamp(0.0, 4095.0)
+                .round() as u16
+        });
         self.with_position_raw(position)
     }
 
-    pub fn with_speed(mut self, speed: u16) -> Self {
+    pub fn with_speed(mut self, speed: Option<u16>) -> Self {
         self.speed = speed;
         self
     }
 
+    fn as_memory(self) -> [Option<u8>; 56] {
+        let memory = [None; 56];
+
+        memory
+    }
+
+    fn as_instructions(self) -> Vec<Result<Instruction, InstructionError>> {
+        self.as_memory()
+            .into_iter()
+            .chain(std::iter::once(None))
+            .enumerate()
+            .fold(
+                (Vec::new(), None::<Vec<u8>>),
+                |(mut instructions, collected), (index, value)| match (collected, value) {
+                    (Some(mut collected), Some(value)) => {
+                        collected.push(value);
+                        (instructions, Some(collected))
+                    }
+                    (None, Some(value)) => (instructions, Some(Vec::<u8>::from([value]))),
+                    (None, None) => (instructions, None),
+                    (Some(collected), None) => {
+                        let start = Address::n(index as u8)
+                            .expect("as_memory must store valid memory addresses.");
+                        instructions.push(Instruction::write(start, collected.clone()));
+                        (instructions, Some(collected))
+                    }
+                },
+            )
+            .0
+    }
+}
+
+impl MoveConfig {
     fn as_write(self) -> Instruction {
         let MoveConfig {
             acceleration,
@@ -97,6 +145,15 @@ impl Servo {
         self.id = new_id;
 
         Ok(response)
+    }
+
+    pub fn write(&self, order: Order, port: &mut Box<dyn SerialPort>) -> Result<(), ServoError> {
+        for instruction in order.as_instructions() {
+            let instruction = instruction?;
+            let command = Command::new(self.id, instruction);
+            serial::packet_tx_rx(command, port)?;
+        }
+        Ok(())
     }
 
     pub fn write_move(
